4e444a75657cd36a63c4c1c6400f4f1ca1fb5848
vishwajeet <vishwa721@gmail.com>
2022-01-14 02:14:53 +0530

    Inner Blog UI

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,37 +1,59 @@
 import { useParams } from "react-router-dom";
 import useFetch from "./useFetch";
 import { useHistory } from "react-router-dom";
+import moment from "moment";
+
+import profile_image from "./images/picture.jpg";
 
 const BlogDetails = () => {
 
-    const { id } = useParams();
-    const { data: blog, isPending, errorMsg } = useFetch("http://localhost:8000/blogs/" + id);
-    const history = useHistory()
+    let blogDate = moment().format('D MMM YYYY');
+  const { id } = useParams();
+  const {
+    data: blog,
+    isPending,
+    errorMsg,
+  } = useFetch("http://localhost:8000/blogs/" + id);
+  const history = useHistory();
 
-    const handleDelete = () => {
-        fetch('http://localhost:8000/blogs/' + blog.id, {
-            method: 'DELETE'
-        }).then(()=> {
-            history.push('/');
-        })
-    }
+  const handleDelete = () => {
+    fetch("http://localhost:8000/blogs/" + blog.id, {
+      method: "DELETE",
+    }).then(() => {
+      history.push("/");
+    });
+  };
 
-    return ( 
-        <div className="blog-details">
-             {isPending && <div>Loading...</div>}
-             {errorMsg && <div>{errorMsg}</div>}
-             {blog && (
-                 <article>
-                     <h2>{ blog.title }</h2>
-                     <p className="blog-author">Written By: { blog.author }</p>
-                     <div className="blog-body">
-                         { `${blog.body}` }
-                     </div>
-                     <button className="blog-delete" onClick={handleDelete}>Delete Me</button>
-                 </article>
-             )}
+  return (
+    <div className="blog-details">
+      <div className="top-profile">
+        <div className="top-profile-picture">
+          <img src={profile_image} alt="profile-photo-top" />
         </div>
-     );
-}
- 
+        <div className="top-profile-name">
+          <p>Vishwajeet Deshmukh</p>
+        </div>
+      </div>
+      {isPending && <div>Loading...</div>}
+      {errorMsg && <div>{errorMsg}</div>}
+      {blog && (
+        <article className="blog-main-content">
+          <div className="main-content-header">
+            <div className="content-title-date">
+              <h2 className="blogdetails-title">{blog.title}</h2>
+              <p className="blogdetails-date">{blogDate}</p>
+            </div>
+            <div className="content-image">
+                <img src={blog.imgsrc} alt="" />
+            </div>
+          </div>
+
+          <div className="blogdetails-body"><p>{`${blog.body}`}</p></div>
+          <button className="blogdetails-delete" onClick={handleDelete}>Delete Me</button>
+        </article>
+      )}
+    </div>
+  );
+};
+
 export default BlogDetails;
