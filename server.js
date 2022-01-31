3f8ce453c9b8738a5e85a7c06a9fd01a1f7411f8
Atharva Joshi <atharvaj2160@gmail.com>
2022-01-31 15:01:14 +0530

    Configured for Depolyment

--- server.js
+++ server.js
@@ -26,6 +26,7 @@ app.use("/", router);
 
 if (process.env.NODE_ENV === "production") {
     const path = require('path')
+    
     app.get("/", (req, res) => {
         app.use(express.static(path.resolve(__dirname, 'client', 'build')))
         res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
