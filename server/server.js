9ed3f3cc966608507ceecaa0c00c562e65da76c0
Atharva Joshi <atharvaj2160@gmail.com>
2022-02-01 23:27:14 +0530

    Configured for Depolyment

--- server/server.js
+++ server/server.js
@@ -24,10 +24,6 @@ app.use(cors());
 app.use(express.json({ limit: "5mb" }));
 app.use("/", router);
 
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-});
-
 if (process.env.NODE_ENV == "production") {
     app.use(express.static("client/build"));
 }
